bce9815677e55e66fbeca4a864578f0267f8b27c
곽효오 <spec3290@gmail.com>
2026-02-24 20:26:20 +0900

    feat::toEntity 메서드 추가

--- todo/src/main/java/com/todolist/todolist/dto/card/CardRequestDto.java
+++ todo/src/main/java/com/todolist/todolist/dto/card/CardRequestDto.java
@@ -1,11 +1,34 @@
 package com.todolist.todolist.dto.card;
 
+
+import com.todolist.todolist.entity.CardEntity;
+import com.todolist.todolist.entity.ListEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CardRequestDto {
     private String title;
     private String content;
     private String emoji;
     private boolean completed;
     private LocalDateTime dueDate;
+    private int position;
+
+    public CardEntity toEntity(ListEntity listEntity) {
+        return CardEntity.builder()
+                .title(this.title)
+                .content(this.content)
+                .completed(this.completed)
+                .emoji(this.emoji)
+                .dueDate(this.dueDate)
+                .todoList(listEntity)
+                .position(this.position)
+                .build();
+    }
 }
