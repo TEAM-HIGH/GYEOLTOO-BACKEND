63438be5910956cb83adb6ba54b57963328d2aa1
곽효오 <spec3290@gmail.com>
2026-02-24 20:27:00 +0900

    feat::카드 엔티티 업데이트 코드 추가

--- todo/src/main/java/com/todolist/todolist/entity/CardEntity.java
+++ todo/src/main/java/com/todolist/todolist/entity/CardEntity.java
@@ -1,17 +1,24 @@
 package com.todolist.todolist.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Getter
+@Builder
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class CardEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
+    private String content;
     private boolean completed;
     private String emoji;
 
@@ -22,4 +29,14 @@ public class CardEntity {
     private ListEntity todoList;
 
     private int position;
+
+    public void update(String title, String content, String emoji, boolean completed, LocalDateTime dueDate, int position) {
+        this.title = title;
+        this.content = content;
+        this.emoji = emoji;
+        this.completed = completed;
+        this.dueDate = dueDate;
+        this.position = position;
+    }
 }
+
