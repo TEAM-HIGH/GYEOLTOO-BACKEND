9298461d7e669c70167e5c799c0dafbe027fa726
곽효오 <spec3290@gmail.com>
2026-03-25 22:08:48 +0900

    feat::배포url cors 설정 추가

--- todo/src/main/java/com/todolist/todolist/global/config/WebConfig.java
+++ todo/src/main/java/com/todolist/todolist/global/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 1. 모든 경로에 대해
-                .allowedOrigins("http://localhost:5173") // 2. 프론트엔드 주소 허용
+                .allowedOrigins("https://gyeoltoo-frontend.vercel.app.com") // 2. 프론트엔드 주소 허용
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS") // 3. 허용할 HTTP 메서드
                 .allowedHeaders("*") // 4. 모든 헤더 허용
                 .allowCredentials(true) // 5. 쿠키/인증 정보 포함 허용 시 필수
