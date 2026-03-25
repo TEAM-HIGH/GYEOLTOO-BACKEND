908bb635fc97070e7ca3a4412aaef28ef4fe1cf4
곽효오 <spec3290@gmail.com>
2026-03-25 22:27:57 +0900

    fix: cors 설정 변경

--- todo/src/main/java/com/todolist/todolist/global/config/WebConfig.java
+++ todo/src/main/java/com/todolist/todolist/global/config/WebConfig.java
@@ -5,14 +5,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // 1. 모든 경로에 대해
-                .allowedOrigins("https://gyeoltoo-frontend.vercel.app.com") // 2. 프론트엔드 주소 허용
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS") // 3. 허용할 HTTP 메서드
-                .allowedHeaders("*") // 4. 모든 헤더 허용
-                .allowCredentials(true) // 5. 쿠키/인증 정보 포함 허용 시 필수
-                .maxAge(3600); // 6. 프리플라이트(Preflight) 요청 캐싱 시간
+        registry.addMapping("/*")
+                .allowedOrigins("https://gyeoltoo-frontend.vercel.app/")
+                .allowedMethods("GET", "POST", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("");
     }
 }
