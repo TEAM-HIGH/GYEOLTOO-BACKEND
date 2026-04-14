d3c7495a1b6734976f168185622e7b3fff3b1021
곽효오 <spec3290@gmail.com>
2026-04-15 00:01:51 +0900

    chore : redis에 refreshtoken 저장 메서드 매개변수 가독성 문제 해결

--- todo/src/main/java/com/todolist/todolist/global/redis/RedisService.java
+++ todo/src/main/java/com/todolist/todolist/global/redis/RedisService.java
@@ -1,12 +1,14 @@
 package com.todolist.todolist.global.redis;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class RedisService {
@@ -15,9 +17,10 @@ public class RedisService {
     private final StringRedisTemplate redisTemplate;
 
     // 1. 데이터 저장 (TTL 설정 포함)
-    public void setValues(String key, String value, Duration duration) {
+    public void setValues(String key, String refreshToken, Duration duration) {
         ValueOperations<String, String> values = redisTemplate.opsForValue();
-        values.set(key, value, duration);
+        values.set(key, refreshToken, duration);
+        log.info("리프레스 토큰 저장 완료! {}", refreshToken);
     }
 
     // 2. 데이터 조회
